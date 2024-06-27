1b3560c51755b7cd791d768a1cae1799c553df20
atulsubedi <atul.work15@gmail.com>
2024-06-27 10:30:27 +0545

    main file updated

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextNode
 
 def main():
-    tn = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    tn = TextNode("This is a text node", "bold", "https://www.google.com")
     print(tn)
 
 
