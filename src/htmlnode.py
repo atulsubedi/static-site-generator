0eb89e10065e6ed0559b11bb52f8b0b3470f4b58
atulsubedi <atul.work15@gmail.com>
2024-06-27 10:27:26 +0545

    initial commit

---
+++ src/htmlnode.py
@@ -0,0 +1,15 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+
+    def props_to_html(self):
+
+
+            
+
