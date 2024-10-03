41b6f2c2b3450d8b28be4f9bb6cdc7be5188e5d0
atul subedi <atul.work15@gmail.com>
2024-10-03 21:22:55 +0545

    Created HtmlNode class for html tags

---
+++ src/htmlnode.py
@@ -0,0 +1,20 @@
+class HTMLNODE:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("no value")
+
+    def props_to_html(self):
+        if self.props is None:
+            return ''
+        string = ''
+        for k in self.props:
+            string += f" {k}={self.props[k]} "
+        return string
+
+    def __repr__(self):
+        return f"tag: {self.tag} value: {self.value} props:{self.props} children: {self.children}"
--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 from textnode import TextNode
+from htmlnode import HTMLNODE
 def main():
     node = TextNode("this is a text node", "bold", "https://www.boot.dev")
     n2 = TextNode("this is a text node", "bold", "https://www.boot.dev")
     print(node.__eq__(n2))
     values = node.__repr__()
     print(values)
+
+    p = HTMLNODE("p", "hi I am SWE", None, {
+    "href": "https://www.google.com",
+    "target": "_blank",
+} )
+    s = p.props_to_html()
+    print(s)
+    values = p.__repr__()
+    print(values)
+
 
 
 
