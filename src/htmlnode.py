c59125e7a364584bd8ee6bfec5e5f05d2917fa4f
atul subedi <atul.work15@gmail.com>
2024-10-04 18:31:42 +0545

    implemented leafnode and its test

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,17 @@ class HTMLNODE:
 
     def __repr__(self):
         return f"tag: {self.tag} value: {self.value} props:{self.props} children: {self.children}"
+
+class LEAFNODE(HTMLNODE):
+    def __init__(self, tag=None, value='' ,props=None):
+        super().__init__(tag, value, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("all leaf nodes must have a value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from htmlnode import HTMLNODE
+from htmlnode import LEAFNODE
 
 class TESTHTMLNODE(unittest.TestCase):
 
@@ -11,6 +12,14 @@ class TESTHTMLNODE(unittest.TestCase):
         })
 
         self.assertEqual(node.props_to_html(), " href=www.test.link.com alt=test_case",)
+
+    def test_to_html_no_children(self):
+        node = LEAFNODE("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_no_tag(self):
+        node = LEAFNODE(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
 if __name__ == "__main__":
     unittest.main()
